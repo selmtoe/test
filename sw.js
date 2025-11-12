bce650d1a41783986bb78f58423b88802f362665
Intai <jhajan634@gmail.com>
2025-11-12 17:23:52 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'tetris-sim-cache-v1';
+const FILES_TO_CACHE = [
+    './index.html',
+    './F/index.html'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
